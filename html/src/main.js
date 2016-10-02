a3702b2f755f6a4a8bf49fa9e25ae645068a326f
Dejan Keroski <keroskidejan@gmail.com>
2016-10-02 15:50:20 +0200

    Index Page code rewrite.

--- html/src/main.js
+++ html/src/main.js
@@ -48,11 +48,11 @@ $(function () {
     });
 
     //navbar menu side
-    $("#open").click(function () {
+    $("#open-menu").click(function () {
         $("#mySidenav").css("width", "250px");
     });
 
-    $("#closebtn").click(function () {
+    $("#close-menu").click(function () {
         $("#mySidenav").css("width", "0");
     });
 
